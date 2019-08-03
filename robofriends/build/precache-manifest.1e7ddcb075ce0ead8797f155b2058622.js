8135c9b5f60d52120cf53b32ba7d69799c41c84b
Andriy Trofimov <webgrandmo@gmail.com>
2019-08-03 21:37:03 +0300

    built file

---
+++ robofriends/build/precache-manifest.1e7ddcb075ce0ead8797f155b2058622.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f2f1ed7ef61aa40228318ae76fec99",
+    "url": "/webgrandmo/robo-friends/index.html"
+  },
+  {
+    "revision": "aa11af8b28d9d8f0aa7b",
+    "url": "/webgrandmo/robo-friends/static/css/2.6cc76bc8.chunk.css"
+  },
+  {
+    "revision": "211d5dee776e48c474c3",
+    "url": "/webgrandmo/robo-friends/static/css/main.fbe9bf8b.chunk.css"
+  },
+  {
+    "revision": "aa11af8b28d9d8f0aa7b",
+    "url": "/webgrandmo/robo-friends/static/js/2.4d43d901.chunk.js"
+  },
+  {
+    "revision": "211d5dee776e48c474c3",
+    "url": "/webgrandmo/robo-friends/static/js/main.e64bf6f0.chunk.js"
+  },
+  {
+    "revision": "4829f22c73cad2a229a5",
+    "url": "/webgrandmo/robo-friends/static/js/runtime~main.8ccb4c4d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/webgrandmo/robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
